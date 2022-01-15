bc6a2600650e2180174d1f5ac833404251659d1f
RennerL <rennerconceicao@gmail.com>
2022-01-15 18:00:33 -0300

    Initial commit

---
+++ Construtores-Propriedades-Delegates-Eventos-Dotnet/ExemploConstrutores/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using ExemploConstrutores.Models;
+
+namespace ExemploConstrutores
+{
+    class Program
+    {
+
+        public delegate void Operacao(int x, int y);
+
+        static void Main(string[] args)
+        {
+            // Exemplo do uso de Evento
+            Matematica m = new Matematica(10, 20);
+            m.Somar();
+
+            // Exemplo do uso de Delegate
+            Operacao op = new Operacao(Calculadora.Somar);
+            //Operacao op = Calculadora.Somar; -> Pode ser chamado dessa outra forma
+            op += Calculadora.Subtrair;
+
+            op.Invoke(10, 10);
+            op(10, 10);
+
+            // Uso de constante
+            const double pi = 3.14;
+            // pi = 0 -> Não funciona, para isso podemos usar uma variável
+            System.Console.WriteLine(pi);
+
+            // Exemplos d Get e Set
+            Data data = new Data();
+            data.SetMes(20);
+
+            data.Mes = 20;
+            System.Console.WriteLine(data.Mes);
+
+            data.Apresentar();
+
+            // Uso do construtor na herança
+            Aluno p2 = new Aluno("Lucas", "Freitas", "Matemática");
+            p2.Apresentar();
+
+            // Uso do construtor privado e padrão Singleton
+            Log Log = Log.GetInstance();
+            Log.PropriedadeLog = "Teste instancia";
+
+            Log log2 = Log.GetInstance();
+            System.Console.WriteLine(log2.PropriedadeLog);
+
+            // Exemplo Construtor
+            Pessoa p1 = new Pessoa();
+            p1.Apresentar();
+        }
+    }
+}
---
+++ Construtores-Propriedades-Delegates-Eventos-Dotnet/README.md
@@ -0,0 +1,28 @@
+# Sobre
+O código foi desenvolvido durante o Curso "Construtores, Propriedades, Delegates e Eventos em .NET" da DIO - [Digital Innovation One](ttps://digitalinnovation.one) Ministrado por [Leonardo Buta](https://github.com/leonardo-buta)
+
+## Temas abordados
+- Construtores (padrão e privado)
+- Getters e Setters
+- Propriedades
+- Modificadores (Readonly, constantes)
+- Delegates e Multi Cast Delegate
+- Eventos
+
+## Como baixar o Código
+````bash
+
+# Clonar o repositório
+$ git clone https://github.com/RennerLopes/Construtores-Propriedades-Delegates-Eventos-Dotnet
+
+# Entrar no diretório
+$ cd Orientacao-a-Objeto-com-CSharp
+
+# Instalar as dependências
+# É necessário ter instalado o .NET
+$ dotnet restore
+
+# Executar o código-fonte
+$ dotnet run
+
+````
